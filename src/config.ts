9e752e4c8ddd8c622afdeb83c4d3453dae6f645c
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-22 11:34:55 +0530

    updated mongo url

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,6 @@ export default {
   networks: {
     ganache: "http://127.0.0.1:8545",
   },
-  mongo_url: "mongodb://localhost:27017/picturama",
   contract_address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
   chain_id: 1337,
 };
--- src/service/db.ts
+++ src/service/db.ts
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
-import config from "../config";
+require("dotenv").config();
+
+const remote_url = `mongodb+srv://Jithin:${process.env.MONGO_PASSWORD}@cluster0.p5d7p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const local_url = `mongodb://localhost:27017/picturama`
 
 export const connectToDB = async () => {
   console.log("Connecting to db...");
-  await mongoose.connect(config.mongo_url);
+  await mongoose.connect(remote_url);
   console.log("Connected to db");
 };
