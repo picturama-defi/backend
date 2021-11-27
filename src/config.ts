1c700163f8e00b94fa4b5680413a890e531454f3
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-27 13:09:22 +0530

    aby change

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export default {
   networks: {
     localhost: "http://127.0.0.1:8545",
   },
-  contract_address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+  contract_address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
   chain_id: 1337,
 };
