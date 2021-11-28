c39d0f78715dd8c777ec803a49c89dab088a3c43
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-28 11:33:44 +0530

    changed config

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ export default {
     localhost: "https://rinkeby.infura.io/v3/cb2e1379ecf24ee7a7e653a2b7d25e7e",
   },
   contract_address: "0x9b751c1d4Eb659850A41d52bf3397077BE0561be",
-  chain_id: 1337,
+  chain_id: 4,
 };
